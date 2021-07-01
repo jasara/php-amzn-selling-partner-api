9a214e1ff8a98ddd30723f16bc388993de5fa5cd
Keith Brink <keith.brink@gmail.com>
2021-07-01 19:47:52 +0300

    Use docsearch

--- config.production.php
+++ config.production.php
@@ -5,6 +5,6 @@ return [
     'production' => true,
 
     // DocSearch credentials
-    'docsearchApiKey' => '',
-    'docsearchIndexName' => '',
+    'docsearchApiKey' => 'c9740329596a085ab4d109017d07a8a3',
+    'docsearchIndexName' => 'phpspa',
 ];
