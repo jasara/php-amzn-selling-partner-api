9ee7a01823516f0a1b8af7a288bc1dd1badd3858
josh8k <jklaristenfeld@gmail.com>
2022-12-07 22:15:39 -0800

    Fixed typo

--- src/DataTransferObjects/Requests/ProductPricing/GetListingOffersBatchRequest.php
+++ src/DataTransferObjects/Requests/ProductPricing/GetListingOffersBatchRequest.php
@@ -4,12 +4,12 @@ namespace Jasara\AmznSPA\DataTransferObjects\Requests\ProductPricing;
 
 use Jasara\AmznSPA\DataTransferObjects\Requests\BaseRequest;
 use Jasara\AmznSPA\DataTransferObjects\Schemas\ProductPricing\ListingOffersRequestListSchema;
-use Jasara\AmznSPA\DataTransferObjects\Schemas\ProductPricing\ListingOfferRequestSchema;
+use Jasara\AmznSPA\DataTransferObjects\Schemas\ProductPricing\ListingOffersRequestSchema;
 use Spatie\DataTransferObject\Attributes\CastWith;
 use Spatie\DataTransferObject\Casters\ArrayCaster;
 
 class GetListingOffersBatchRequest extends BaseRequest
 {
-    #[CastWith(ArrayCaster::class, itemType: ListingOfferRequestSchema::class)]
+    #[CastWith(ArrayCaster::class, itemType: ListingOffersRequestSchema::class)]
     public ListingOffersRequestListSchema $requests;
 }
