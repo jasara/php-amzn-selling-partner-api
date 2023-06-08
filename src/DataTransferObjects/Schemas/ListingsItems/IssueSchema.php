37e233926ac0dfddf6ac710499dfa269fb1284f6
Keith Brink <keith.brink@gmail.com>
2023-06-08 17:10:55 -0400

    make attribute names nullable

--- src/DataTransferObjects/Schemas/ListingsItems/IssueSchema.php
+++ src/DataTransferObjects/Schemas/ListingsItems/IssueSchema.php
@@ -8,11 +8,9 @@ use Spatie\DataTransferObject\DataTransferObject;
 class IssueSchema extends DataTransferObject
 {
     public string $code;
-
     public string $message;
 
     #[StringEnumValidator(['ERROR', 'WARNING', 'INFO'])]
     public string $severity;
-
-    public array $attribute_names;
+    public ?array $attribute_names;
 }
