1dfaedad9772691c69db6ebbeb8188f09ae9aaf6
Keith Brink <keith.brink@gmail.com>
2024-01-15 11:03:35 +0300

    Make bill of lading payload nullable

--- src/DataTransferObjects/Responses/FulfillmentInbound/GetBillOfLadingResponse.php
+++ src/DataTransferObjects/Responses/FulfillmentInbound/GetBillOfLadingResponse.php
@@ -7,5 +7,5 @@ use Jasara\AmznSPA\DataTransferObjects\Schemas\FulfillmentInbound\BillOfLadingDo
 
 class GetBillOfLadingResponse extends BaseResponse
 {
-    public BillOfLadingDownloadURLSchema $payload;
+    public ?BillOfLadingDownloadURLSchema $payload;
 }
