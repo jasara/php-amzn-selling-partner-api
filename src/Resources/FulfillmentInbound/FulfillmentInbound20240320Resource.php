b07ce1a17d19794626beea2eb40704ce072201a5
Abdelmajied <abdelmajied.a@jasaratech.com>
2024-09-04 14:06:26 +0300

    Fix the endpoint of packing group items

--- src/Resources/FulfillmentInbound/FulfillmentInbound20240320Resource.php
+++ src/Resources/FulfillmentInbound/FulfillmentInbound20240320Resource.php
@@ -515,8 +515,6 @@ class FulfillmentInbound20240320Resource implements ResourceContract
         #[RuleValidator(['size:38'])]
         string $inbound_plan_id,
         #[RuleValidator(['size:38'])]
-        string $packing_option_id,
-        #[RuleValidator(['size:38'])]
         string $packing_group_id,
         #[RuleValidator(['integer', 'min:1', 'max:100'])]
         ?int $page_size = null,
@@ -527,7 +525,6 @@ class FulfillmentInbound20240320Resource implements ResourceContract
             ->responseClass(ListPackingGroupItemsResponse::class)
             ->get(
                 $this->endpoint . self::BASE_PATH . 'inboundPlans/' . $inbound_plan_id
-                . '/packingOptions/' . $packing_option_id
                 . '/packingGroups/' . $packing_group_id
                 . '/items',
                 array_filter([
