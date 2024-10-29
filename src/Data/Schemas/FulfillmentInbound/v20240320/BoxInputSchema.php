4ab972a908de0af441a7cd5d63c8a751f354b6c0
abdelmajied94abusuliman <115414116+abdelmajied94abusuliman@users.noreply.github.com>
2024-10-29 18:40:31 +0300

    Replace box content with item input (#75)

--- src/Data/Schemas/FulfillmentInbound/v20240320/BoxInputSchema.php
+++ src/Data/Schemas/FulfillmentInbound/v20240320/BoxInputSchema.php
@@ -12,7 +12,7 @@ class BoxInputSchema extends BaseSchema
         #[RuleValidator(['min:1', 'max:1024'])]
         public ?string $box_id,
         public BoxContentInformationSource $content_information_source,
-        public BoxContentSchemaList $contents,
+        public ItemInputSchemaList $items,
         public DimensionsSchema $dimensions,
         #[RuleValidator(['integer', 'min:1', 'max:10000'])]
         public int $quantity,
