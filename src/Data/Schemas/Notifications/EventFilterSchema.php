17be10d3e38091f18acc1932905ad42c367dbf06
abdelmajied94abusuliman <abdelmajied.abusuliman@gmail.com>
2025-05-15 14:45:13 +0300

    fix: event filter schema

--- src/Data/Schemas/Notifications/EventFilterSchema.php
+++ src/Data/Schemas/Notifications/EventFilterSchema.php
@@ -8,11 +8,11 @@ use Jasara\AmznSPA\Data\Schemas\BaseSchema;
 class EventFilterSchema extends BaseSchema
 {
     public function __construct(
-        public ?AggregationSettingsSchema $aggregation_settings,
-        public ?array $marketplace_ids,
         #[StringEnumValidator(['ANY_OFFER_CHANGED'])]
         public string $event_filter_type,
-        public array $order_change_types
+        public array $order_change_types,
+        public ?AggregationSettingsSchema $aggregation_settings,
+        public ?array $marketplace_ids
     ) {
     }
 }
