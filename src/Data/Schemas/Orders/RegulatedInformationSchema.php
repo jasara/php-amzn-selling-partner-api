e588c7d1c0902d0a4d5a882fefeb6564bc86782f
abdelmajied94abusuliman <abdelmajied.abusuliman@gmail.com>
2025-07-15 15:18:51 +0300

    wip

--- src/Data/Schemas/Orders/RegulatedInformationSchema.php
+++ src/Data/Schemas/Orders/RegulatedInformationSchema.php
@@ -5,7 +5,7 @@ namespace Jasara\AmznSPA\Data\Schemas\Orders;
 use Jasara\AmznSPA\Data\Base\TypedCollection;
 
 /**
- * @template-extends TypedCollection<FieldsScehma>
+ * @template-extends TypedCollection<FieldListScehma>
  */
 class RegulatedInformationSchema extends TypedCollection
 {
--- src/Data/Schemas/Orders/RegulatedOrderVerificationStatusSchema.php
+++ src/Data/Schemas/Orders/RegulatedOrderVerificationStatusSchema.php
@@ -9,8 +9,8 @@ class RegulatedOrderVerificationStatusSchema extends BaseSchema
     public function __construct(
         public string $status,
         public string $external_reviewer_id,
-        public ?string $rejection_reason_id = null,
         public ValidRejectionReasonsListSchema $valid_rejection_reasons,
+        public ?string $rejection_reason_id = null,
     ) {
     }
 }
